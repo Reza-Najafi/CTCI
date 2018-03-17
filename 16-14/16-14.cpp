0a41d72f3f48d3cea18c0e1085282f87e2fce462
Reza <Reza@Rezas-MacBook-Pro.local>
2018-03-17 14:07:50 -0400

    Improved 16-14 using tuples

--- 16-14/16-14.cpp
+++ 16-14/16-14.cpp
@@ -1,19 +1,17 @@
 
 #include <iostream>
-#include <utility>
 #include <map>
 #include <set>
 #include <vector>
-#include <cfloat>
 #include <cmath>
-
+#include <tuple>
 using namespace std;
 class BestLine {
 public:
     typedef  pair<int,int> Point;
-    typedef  pair<float, float> Line;
-    typedef  float InfLine;
-    
+    typedef  tuple<int, float, float> Line;
+    //typedef  float InfLine;
+
     void insert(vector<Point>& vp) {
         int sz = vp.size();
         for(int i = 0; i < sz; i++) {
@@ -21,47 +19,43 @@ public:
                 //find the slope of the vp[i] vp[j]
                 // enter the pair into the map
                 float s = slope(vp[i], vp[j]);
-                
+
                 if(!isinf(s)) {
                     float b = vp[i].second - s*vp[i].first;
-                    auto line = make_pair(s,b);
+                    auto line = make_tuple(1,s,b);
                     if(m.find(line) == m.end()) {
                         set<Point> * v = new set<Point>;
                         m.insert(make_pair(line,v));
-                    } 
+                    }
                     m[line]->insert(vp[i]);
                     m[line]->insert(vp[j]);
                 } else {
-                    auto line = vp[i].first;
-                    if(im.find(line) == im.end()) {
+                    float b = -vp[i].first;
+                    auto line = make_tuple(0,1, b);
+                    if(m.find(line) == m.end()) {
                         set<Point> * v = new set<Point>;
-                        im.insert(make_pair(line,v));
-                    } 
-                    im[line]->insert(vp[i]);
-                    im[line]->insert(vp[j]);
+                        m.insert(make_pair(line,v));
+                    }
+                    m[line]->insert(vp[i]);
+                    m[line]->insert(vp[j]);
                 }
             }
         }
     }
-    
+
     set<Point>* find(){
-        int max = 0; 
+        int max = 0;
         set<Point>* ps = m.begin()->second;
         for(auto& s: m){
             if(s.second->size() > max){ max = s.second->size(); ps = s.second;}
         }
-        for(auto& s:im){
-            if(s.second->size() > max){ max = s.second->size(); ps = s.second;}
-        }
         return ps;
     }
-    
+
 private:
     typedef map<Line,set<Point>*> MapType;
-    typedef map<InfLine,set<Point>*> InfMapType;
     MapType m;
-    InfMapType im;
-    
+
     float slope(Point& p1, Point&p2){
             return ((float)p2.second - (float)p1.second)/((float)p2.first - (float)p1.first);
     }
@@ -72,7 +66,8 @@ private:
 int main()
 {
     BestLine bl;
-    vector<BestLine::Point> v = {make_pair(0,0),make_pair(-1,1), make_pair(1,2), make_pair(2,5), make_pair(3,6), make_pair(1,3), make_pair(1,4), make_pair(1,5), make_pair(4,8)};
+    vector<BestLine::Point> v = {make_pair(3,8), make_pair(0,0),make_pair(-1,1), make_pair(1,2), make_pair(2,5),
+    make_pair(3,6), make_pair(1,3), make_pair(1,4), make_pair(1,5), make_pair(4,8), make_pair(5,10)};
     bl.insert(v);
     auto out = bl.find();
     for(auto& p: *out){
@@ -82,5 +77,3 @@ int main()
 
     return 0;
 }
-
-
