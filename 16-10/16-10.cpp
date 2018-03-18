c6729447ae41b175fc17caaf649e33c9afa30ca1
Reza <Reza@Rezas-MacBook-Pro.local>
2018-03-18 01:45:04 -0400

    Done with 16-10

--- 16-10/16-10.cpp
+++ 16-10/16-10.cpp
@@ -2,21 +2,37 @@
 #include <iostream>
 #include <vector>
 #include <map>
-#include <vector>
-
 
 using namespace std;
 class MostLivingPeople {
 public:
-    void getMostLivingYear(pair<vector<int>, vector<int>>& v) {
-
+    pair<int,int> getMostLivingYear(pair<vector<int>, vector<int>>& v) {
+        sort(v.first);
+        vector<int> y(v.first.size(),0);
+        int i = 0;
+        for(auto& e:y){ e = ++i;}
+        for(auto& e:v.second){
+            int idx = lowerBound(v.first,e);
+            if(idx >=0){
+                while(idx < y.size() ){y[idx++]--;}
+            }
+        }
+        int max = 0;
+        int retIdx = -1;
+        for(int i = 0;i< y.size(); i++){
+            if(y[i]>max){
+                max = y[i];
+                retIdx = i;
+            }
+        }
+        return make_pair(v.first[retIdx],max);
     }
 
-    int upperBound(vector<int>& a , int v) {
-        return upperBound(a,0,a.size()-1,v);
+    int lowerBound(vector<int>& a , int v) {
+        return lowerBound(a,0,a.size()-1,v);
     }
 
-    int upperBound(vector<int>& a , int l, int h, int v){
+    int lowerBound(vector<int>& a , int l, int h, int v){
         if(a[h]<v){return -1;}
         int mid = (l+h)/2;
         if(mid-1>=0){
@@ -28,11 +44,11 @@ public:
                 return mid;
             }
         }
-        int idx = upperBound(a,l,mid-1,v);
+        int idx = lowerBound(a,l,mid-1,v);
         if(idx != -1){
             return idx;
         }
-        idx= upperBound(a,mid+1,h,v);
+        idx= lowerBound(a,mid+1,h,v);
         if(idx != -1){
             return idx;
         }
@@ -75,15 +91,20 @@ private:
 int main()
 {
     vector<int> test = {-10,10,1,4,3,7,1,9,7,0,4,67,13,2,1,0};
-    vector<int> birth = {1,3,6 ,8 ,19,37,65};
-    vector<int> death = {6,8,18,28,56,39,99};
+    vector<int> birth = {21,12, 25,1,10, 29,3 ,19,9 , 4 ,8,20 };
+    vector<int> death = {45,14, 56,9,39, 67,28,56,45,18,28,38};
     MostLivingPeople mlp;
     mlp.sort(test);
+#if defined TEST
     for(auto&i:test){
         cout << i<<" , ";
     }
     cout << endl;
     int val = 5;
-    cout << "Upper bound of "<< val << " is at index "<< mlp.upperBound(test,val)<< endl;
+    cout << "Upper bound of "<< val << " is at index "<< mlp.lowerBound(test,val)<< endl;
+#endif
+    auto d = make_pair(birth, death);
+    auto res = mlp.getMostLivingYear(d);
+    cout << "max number of years " << res.first << " occured "<<res.second<< " times"<< endl;
     return 0;
 }
