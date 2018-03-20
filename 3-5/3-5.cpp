26cbe6a4e9509519a4a28db5c436db5deeefdca0
Reza <Reza@Rezas-MacBook-Pro.local>
2018-03-20 14:33:33 -0400

    Done with 3-5

--- 3-5/3-5.cpp
+++ 3-5/3-5.cpp
@@ -6,28 +6,39 @@ using namespace std;
 void sortStack(stack<int>& s){
     stack<int> s1;
     int sz = s.size();
-    while(sz-->=0){
+    int i = 0;
+    while(i++ < sz){
         int min = s.top();
-        while(!s.empty()) {
+        while(s.size()> (i-1)) {
             if(s.top()< min) {
                 min = s.top();
             }
             s1.push(s.top());
             s.pop();
         }
+        s.push(min);
+        bool foundMin = false;
         while(!s1.empty()) {
-            if(s1.top() != min){
+            if(s1.top() != min || foundMin){
                 s.push(s1.top());
+            }
+            if(s1.top() == min){
+                foundMin = true;
             }
             s1.pop();
         }
-        s.push(min);
     }
+    while(!s.empty()){
+        s1.push(s.top());
+        s.pop();
+    }
+    s = s1;
 }
 
 int main()
 {
     int arr[] = {1,5,0,-2,4,2,3,0,1,7,9,-5};
+    cout << sizeof(arr)/sizeof(arr[0]) << endl;
     stack<int> s;
     for(auto& i : arr){
         s.push(i);
