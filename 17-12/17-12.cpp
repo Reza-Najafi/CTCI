b7273d19df4ba2c395127db9aacd0e1b67af062b
Reza-Najafi <najafi.sreza@gmail.com>
2018-03-21 18:47:30 -0400

    Done with 7.12!

---
+++ 17-12/17-12.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include "Tree.h"
+using namespace std;
+
+
+
+Node* rec(Node*n , int p) {
+
+    Node* l = NULL, *r = NULL;
+    if(n->a[0]) {
+        l = rec(n->a[0],0);
+    }
+    if(n->a[1]) {
+        r = rec(n->a[1],1);
+    }
+    Node* ret = NULL;
+    n->a[1] = r;
+    n->a[0] = l;
+    if(l)l->a[1] = n;
+    if(r)r->a[0] = n;
+    if(p==0){
+        if(r) {
+            ret = r;
+            // continue on the right wing until the leaf
+            while(ret->a[1]){
+                ret = ret->a[1];
+            }
+        } else {
+            ret = n;
+        }
+    }
+
+    if(p==1){
+        if(l) {
+            ret = l;
+            // continue on the left wing until the leaf
+            while(ret->a[0]){
+                ret = ret->a[0];
+            }
+        } else {
+            ret = n;
+        }
+    }
+
+    return ret;
+}
+Node* straigten(Node* n){
+    rec(n,1);
+    while(n->a[0]){n= n->a[0];}
+    return n;
+};
+
+
+
+int main()
+{
+    #if 1
+    Node* n3 = ct(3, ct(2,ct(1)), ct(4));
+    Node* n6 = ct(6,nullptr,ct(7));
+    Node* n8 = ct(8,n6);
+    Node* n12 = ct(12,ct(11,ct(10)),ct(13));
+    Node* n14 = ct(14,n12,ct(20));
+    Node* n13 = ct(13,nullptr, n14);
+    Node* n9  = ct(9,n8,n13);
+    Node* n  = ct(5,n3,n9);
+    #endif
+    Node* tree_root = n;
+    printTree(tree_root);
+    Node* res = straigten(tree_root);
+    do{
+        cout << res->v << " , ";
+        res=res->a[1];
+    }while(res);
+
+
+    return 0;
+}
---
+++ 17-12/Tree.h
@@ -0,0 +1,57 @@
+#include <string>
+#include <iostream>
+#include <queue>
+struct Node{
+    int v;
+    Node* a[2] = {NULL};
+    Node(int val) {v =val;}
+    Node(int val, Node* n0, Node* n1) {a[0] = n0; a[1]=n1; v = val;}
+};
+// create_tree function
+Node* ct(int v, Node* nl=NULL, Node* nr=NULL) {
+    Node* n = new Node(v, nl, nr);
+    return n;
+};
+
+struct NodeWrapper  {
+    Node* n;
+    int l;// keeps the level
+    int p;//lefat what width did the parent were appear on the screen
+    int lr;// left child or right child -1 or 1
+    NodeWrapper(Node* node, int level, int pos, int left_right) :n(node),l(level),p(pos),lr(left_right) {}
+};
+
+void printTree(Node* n) {
+    int curr_level = 0;
+    std::queue<NodeWrapper> s;
+    int rootWidth = 4*20;
+    s.push(NodeWrapper(n,0,rootWidth,0));
+    int currLocationOnlevel = 0;
+    char delim = '.';
+    while(!s.empty()) {
+        NodeWrapper thisN = s.front();
+        int new_level = thisN.l;
+        if(new_level != curr_level){
+            curr_level = new_level;
+            int remaining =2*rootWidth - currLocationOnlevel; 
+            if(remaining>0)std::cout << std::string(remaining,delim);
+            std::cout << std::endl;
+            currLocationOnlevel= 0;
+        } 
+        int children_gap = rootWidth/(1 << (thisN.l)); 
+        int thisLoc = thisN.p+thisN.lr*children_gap - currLocationOnlevel;
+        if (thisLoc < 0) {thisLoc = 1;}
+        currLocationOnlevel += thisLoc;
+        std::cout << std::string(thisLoc,delim) << thisN.n->v ;
+        if(thisN.n->a[0]) {
+            s.push(NodeWrapper(thisN.n->a[0],thisN.l+1,currLocationOnlevel,-1));
+        }
+        if(thisN.n->a[1]){
+            s.push(NodeWrapper(thisN.n->a[1],thisN.l+1,currLocationOnlevel,1));
+        }
+        s.pop();
+    }
+    int remaining =2*rootWidth - currLocationOnlevel; 
+    if(remaining>0)std::cout << std::string(remaining,delim);
+    std::cout << std::endl;
+}
