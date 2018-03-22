0bc47dc6aba317f62552ae6b21459b9feb1209c0
Reza-Najafi <najafi.sreza@gmail.com>
2018-03-21 20:31:04 -0400

    Updated 4-12

--- 4-12/4-12.cpp
+++ 4-12/4-12.cpp
@@ -1,1 +1,13 @@
-#include "Tree.h"
+#include "../utilities/Tree.h"
+#include <iostream>
+using namespace std;
+int main() {
+    Node* n1 = ct(1,NULL,ct(3));
+    Node* n2 = ct(2,ct(1),ct(-1));
+    Node* n4 = ct(4,NULL,ct(2));
+    Node* n5 = ct(5,ct(-3),NULL);
+    Node* n6 = ct(6,n1,n2);
+    Node* n_5 = ct(-5,n4,n5);
+    Node* root = ct(2,n6,n_5);
+    cout << n->v;
+}
