6e3df9cd63d9c76f78446816bde35e1b9f7aeda9
Reza <Reza@Rezas-MacBook-Pro.local>
2018-04-07 00:45:50 -0400

    Added comments

--- 17-18/17-18.cpp
+++ 17-18/17-18.cpp
@@ -1,3 +1,10 @@
+/******************************************************************************************************
+* Seyedreza Najafi , March-2018
+* Solution to the following problem from the "Cracking the Coding Interview" book, 6th edition
+*
+* 17.18 Shortest Supersequence
+*
+******************************************************************************************************/
 #include <iostream>
 #include <map>
 #include <vector>
